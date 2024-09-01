9e5007d5b786a250ea2ce73ff51aa88d3e0f159a
Matthew Ngo <ngonhuthanhtrung1409@gmail.com>
2024-09-01 07:05:32 +0700

    Update template

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,3 @@
-// import { FlatCompat } from '@eslint/eslintrc';
-
-// const compat = new FlatCompat({
-//   baseDirectory: import.meta.url,
-// });
-
-// export default [
-//   ...compat.config({
-//     parser: '@typescript-eslint/parser',
-//     plugins: ['@typescript-eslint', 'prettier'],
-//     extends: [
-//       'plugin:@typescript-eslint/recommended',
-//       'plugin:prettier/recommended',
-//     ],
-//     rules: {
-//       '@typescript-eslint/no-require-imports': 'off',
-//     },
-//   }),
-// ];
-
-
 export default {
   parser: '@typescript-eslint/parser',
   extends: [
